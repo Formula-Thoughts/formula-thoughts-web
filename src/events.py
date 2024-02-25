975aa65053b8f1d0076ea3b86971553da2544464
aidan <aidanwilliamgannon@gmail.com>
2024-02-25 17:34:23 +0000

    add global exception handler in event runner

--- src/events.py
+++ src/events.py
@@ -18,15 +18,20 @@ class EventRunner:
         self.__event_handlers = event_handlers
 
     def run(self, event: dict):
-        for message in event['Records']:
-            event_type = message['messageAttributes']['messageType']['stringValue']
-            body = message['body']
-            watch = self.__event_handlers
-            matching_handlers = list(filter(lambda x: f"{x.event_type.__name__}" == event_type, self.__event_handlers))
-            if len(matching_handlers) is 0:
-                raise EventNotFoundException(f"{event_type} does not match any found handlers")
-            matching_handlers[0].run(event=body)
-
+        try:
+            for message in event['Records']:
+                event_type = message['messageAttributes']['messageType']['stringValue']
+                body = message['body']
+                self.__logger.add_global_properties(properties={"event_type": event_type})
+                watch = self.__event_handlers
+                matching_handlers = list(filter(lambda x: f"{x.event_type.__name__}" == event_type, self.__event_handlers))
+                if len(matching_handlers) is 0:
+                    raise EventNotFoundException(f"{event_type} does not match any found handlers")
+                matching_handlers[0].run(event=body)
+        except Exception as e:
+            self.__logger.log_error(message="event runner captured exception")
+            self.__logger.log_exception(exception=e)
+            raise e
 
 class EventHandlerBase(ABC):
 
